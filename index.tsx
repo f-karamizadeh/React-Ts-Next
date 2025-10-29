cd2f6c1294786d09b5ccd47cbab3539598faacc8
f.karamizadeh@yahoo.com <f-karamizadeh>
2025-10-29 13:10:16 +0100

    Task-Ts

---
+++ index.tsx
@@ -0,0 +1,12 @@
+import { Task } from "./types";
+
+
+function Component() {
+  return (
+    <div>
+      <h2>React & Typescript</h2>
+      <h2>Task List</h2>
+    </div>
+  );
+}
+export default Component;
